7aca3aadd66cd0213ff090484cc85e6401631e54
toluwalase <technogodwr@gmail.com>
2023-12-05 02:50:37 +0100

    movind to cli

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 const app = Vue.createApp({
-  template: `
-    <div v-if="">
-   <div v-if="showBooks">
-   <h2>{{title}} - {{author}} - {{age}}</h2>
-   </div>
-    <button @click="toggleShowBooks">{{this.showBooks==true? "hidebooks": "showbooks"}}</button>
-    <button @click="age--">Decrease Age</button>
-    <div @click="changeTitle"> change</div>
-    </div>
-    `,
+  template: ``,
   data() {
     return {
-      title: "the final stream",
-      author: "brandon smith",
-      age: "45",
+      url: "http://portfolical.web.app" /*"http://www.thenetninja.co.uk"*/,
       showBooks: true,
+      books: [
+        { title: "the final empire", author: "brandon smith", isFav: true },
+        { title: "the way of the wind", author: "barry allen", isFav: false },
+        {
+          title: "the way of kings",
+          author: "patrick the trickster",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     changeTitle() {
       this.title = "Words of romance";
     },
-    toggleShowBooks(){
-        this.showBooks = !this.showBooks
-    }
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    toggleFav(e, book) {
+      book.isFav = !book.isFav;
+    },
+  },
+
+  computed: {
+    filteredFavs() {
+      return this.books.filter((book) => book.isFav);
+    },
   },
 });
 
